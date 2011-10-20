a930621caff366b8b7e9ee6850c74d7bb2763337
Simon Gellis <simongellis@gmail.com>
2011-10-19 23:59:45 -0400

    Implemented an unfinished Naive Strategy
    Signed-off-by: Simon Gellis <simongellis@gmail.com>

--- SLDP/NaiveStrategy.cpp
+++ SLDP/NaiveStrategy.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 #include <vector>
 #include "NaiveStrategy.h"
 #include "Node.h"
@@ -7,6 +9,8 @@ using namespace std;
 
 namespace SLDP
 {
+	NaiveStrategy::NaiveStrategy(TrainMode mode, const string& start)
+		: mode(mode), start(start) {}
 	Result NaiveStrategy::Execute(Track* track)
 	{
 		const string obstacleLabels[] = {"L1A", "AB", "BC", "CD", "DR1", "CF", "FG", "GD", "AJ", "IB", "FN", "MG", "L2I", "IJ", "JK", "KL", "LM", "MN", "NO", "OP", "PR2", "KS", "QL", "OW", "VX", "L3Q", "QR", "RS", "SU", "UV", "VW", "WR3", "RU"};
@@ -25,11 +29,58 @@ namespace SLDP
 			obstacles[ blocked[x]->getLabel() ] = true;
 		}
 
-		if (!(obstacles["JK"] || obstacles["SU"]))
+		if (mode == NORMAL)
 		{
-			// derp
+			if (start == "L1")
+			{
+				return NOPATH;
+			}
+			if (start == "L2")
+			{
+				track->getFirstNode("K")->setCurrentEdge(RIGHT, !(obstacles["JK"] || obstacles["SU"]));
+				if (obstacles["JK"] || (obstacles["SU"] && (obstacles["LM"] || obstacles["NO"]))) return NOPATH;
+				else return SUCCESS;
+			}
+			if (start == "L3")
+			{
+				track->getFirstNode("K")->setCurrentEdge(RIGHT, obstacles["SU"] && !(obstacles["LM"] || obstacles["NO"]));
+				track->getFirstNode("R")->setCurrentEdge(RIGHT, obstacles["SU"] && (obstacles["LM"] || obstacles["NO"]));
+				track->getFirstNode("U")->setCurrentEdge(LEFT, track->getFirstNode("R")->edgeNotDefault(RIGHT));
+				if (track->getFirstNode("R")->edgeNotDefault(LEFT) || obstacles["RU"]) return NOPATH;
+				else return SUCCESS;
+			}
 		}
-		return SUCCESS;
+		if (mode == REVERSE)
+		{
+			if (start == "L1")
+			{
+				track->getFirstNode("A")->setCurrentEdge(RIGHT, obstacles["BC"] || obstacles["CD"] && obstacles["CF"]);
+				track->getFirstNode("K")->setCurrentEdge(RIGHT, obstacles["LM"]);
+				track->getFirstNode("C")->setCurrentEdge(RIGHT, obstacles["CD"]);
+				track->getFirstNode("F")->setCurrentEdge(RIGHT, obstacles["BC"]);
+				if (track->getFirstNode("A")->edgeNotDefault(RIGHT) && (obstacles["JK"] || obstacles["LM"] && obstacles["SU"])) return NOPATH;
+				else return SUCCESS;
+			}
+			if (start == "L2")
+			{
+				track->getFirstNode("A")->setCurrentEdge(RIGHT, obstacles["JK"] || obstacles["LM"] && obstacles["SU"]);
+				track->getFirstNode("K")->setCurrentEdge(RIGHT, obstacles["LM"]);
+				track->getFirstNode("C")->setCurrentEdge(RIGHT, obstacles["CD"]);
+				track->getFirstNode("F")->setCurrentEdge(RIGHT, !(obstacles["JK"] || obstacles["LM"]) && obstacles["NO"]);
+				if (track->getFirstNode("A")->edgeNotDefault(RIGHT) && (obstacles["BC"] || obstacles["CD"] && obstacles["CF"])) return NOPATH;
+				else return SUCCESS;
+			}
+			if (start == "L3")
+			{
+				track->getFirstNode("K")->setCurrentEdge(RIGHT, obstacles["SU"] && !(obstacles["LM"]));
+				track->getFirstNode("R")->setCurrentEdge(RIGHT, obstacles["SU"]);
+				track->getFirstNode("F")->setCurrentEdge(RIGHT, track->getFirstNode("K")->edgeNotDefault(RIGHT));
+				track->getFirstNode("U")->setCurrentEdge(LEFT, obstacles["SU"]);
+				if (obstacles["LM"] && obstacles["SU"] && obstacles["RU"]) return NOPATH;
+				else return SUCCESS;
+			}
+		}
+		return NOPATH;
 	}
 
 	void NaiveStrategy::Destruct()
